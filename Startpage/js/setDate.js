b0deae71b04d0d37cc6d115c9008649416648ad1
scripth3x <88942282+scripth3x@users.noreply.github.com>
2021-08-14 17:54:03 +0000

    Add files via upload

---
+++ Startpage/js/setDate.js
@@ -0,0 +1,22 @@
+const updateDate = () => {
+  const completeDate = new Date();
+  const timeOptions = {
+    hour    : "numeric",
+    minute  : "numeric",
+    hour12  : CONFIG.hour12
+  }
+  const timeFormat = 
+    new Intl.DateTimeFormat(CONFIG.locale, timeOptions).format(completeDate);
+  const dateOptions = {
+    weekday : "long",
+    month   : "long",
+    day     : "numeric"
+  }
+  const dateFormat = 
+    new Intl.DateTimeFormat(CONFIG.locale, dateOptions).format(completeDate);
+
+  currentTime.innerHTML = timeFormat;
+  currentDate.innerHTML = dateFormat;
+}
+
+setInterval(updateDate, 1000);
